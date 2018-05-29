a85b70b1225c434c94c49397773475e8f7596481
Karan Desai <karandesai281196@gmail.com>
2018-05-29 11:24:08 +0530

    Add an output fc in controller.

--- ntm.py
+++ ntm.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 from ntm_modules.controller import NTMController
@@ -14,22 +15,29 @@ class NTM(nn.Module):
                  memory_unit_size,
                  num_heads):
         super().__init__()
-        self.controller = NTMController(input_size, output_size,
-                                        state_size=controller_size)
+        self.controller = NTMController(
+            input_size, controller_size, output_size
+            read_data_size=controller_size + num_heads * memory_unit_size)
+        
         self.memory = NTMMemory(memory_units, memory_unit_size)
-        self.heads = []
+        self.num_heads = num_heads
+        self.heads = nn.ModuleList([])
         for head in range(num_heads):
             self.heads += [
                 NTMHead('r', controller_size, key_size=memory_unit_size),
                 NTMHead('w', controller_size, key_size=memory_unit_size)
             ]
 
-    @classmethod
-    def init_from_modules(cls, controller, memory, heads):
-        cls.controller = controller
-        cls.memory = memory
-        cls.heads = heads
-        return cls
+        self.prev_head_weights = []
+        self.reset()
+
+    def reset(self, batch_size=1):
+        self.memory.reset(batch_size)
+        self.controller.reset(batch_size)
+        self.prev_head_weights = []
+        for i in range(len(self.heads)):
+            prev_weight = torch.zeros(batch_size, self.memory.n)
+            self.prev_head_weights.append(prev_weight)
 
     def forward(self, in_data):
         pass
--- ntm_modules/controller.py
+++ ntm_modules/controller.py
@@ -3,25 +3,27 @@ from torch import nn
 
 
 class NTMController(nn.Module):
-    def __init__(self, input_size, output_size, state_size):
+    def __init__(self, input_size, state_size, output_size, read_data_size, output_size):
         super().__init__()
         self.input_size = input_size
-        self.output_size = output_size
         self.state_size = state_size
+        self.output_size = output_size
+        self.read_data_size = read_data_size
 
         self.controller_net = nn.LSTMCell(input_size, state_size)
-
-        # fc layer transforming short-term state (h) to output vector
-        self.out_fc = nn.Linear(state_size, output_size)
-
+        self.out_net = nn.Linear(read_data_size, output_size)
         self.h_state = torch.Tensor(1, state_size)
         self.c_state = torch.Tensor(1, state_size)
         self.reset()
 
     def forward(self, in_data):
         self.h_state, self.c_state = self.controller_net(in_data, (self.h_state, self.c_state))
-        output = self.out_fc(self.h_state)
-        return output, self.c_state
+        return self.h_state, self.c_state
+
+    def output(read_data):
+        complete_state = torch.cat([self.c_state] + read_data, dim=1)
+        output = self.out_net(complete_state)
+        return output
 
     def reset(self, batch_size=1):
         self.h_state = torch.Tensor(batch_size, self.state_size)
