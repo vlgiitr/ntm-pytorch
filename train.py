01da384e62e009e7cab8ff2212affb7d0ebe7d6a
Karan Desai <karandesai281196@gmail.com>
2018-06-18 15:29:47 +0530

    Initialize ntm + copy task specific variables in train script.

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import json
+
 import torch
 from torch import nn, optim
 
@@ -7,4 +9,29 @@ from ntm.args import get_parser
 
 
 args = get_parser().parse_args()
-print(args)
+
+# ----------------------------------------------------------------------------
+# -- initialize dataset, model, criterion and optimizer
+# ----------------------------------------------------------------------------
+
+task_params = json.load(open(args.task_json))
+dataset = CopyDataset(task_params)
+
+ntm = NTM(input_size=task_params['seq_width'] + 1,
+		  output_size=task_params['seq_width'],
+		  controller_size=task_params['controller_size'],
+		  memory_units=task_params['memory_units'],
+		  memory_unit_size=task_params['memory_unit_size'],
+		  num_heads=task_params['num_heads'])
+
+criterion = nn.BCELoss()
+optimizer = optim.RMSProp(ntm.parameters(),
+						  lr=args.lr,
+						  alpha=args.alpha,
+						  momentum=args.momentum)
+
+# ----------------------------------------------------------------------------
+# -- basic training loop 
+# ----------------------------------------------------------------------------
+
+# todo
